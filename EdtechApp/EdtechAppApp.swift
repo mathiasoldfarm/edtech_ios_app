1f520bf2042396590b7df61f60faa9efefac3e31
Mathias Gammelgaard <webudviklerendk@gmail.com>
2021-05-20 09:05:53 +0200

    Initial Commit

---
+++ EdtechApp/EdtechAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdtechAppApp.swift
+//  EdtechApp
+//
+//  Created by MATHIAS GAMMELGAARD on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdtechAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
